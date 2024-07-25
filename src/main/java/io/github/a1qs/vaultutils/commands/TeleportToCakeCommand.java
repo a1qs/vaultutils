9a444cacfea4a8ca60726e8b6c386f0e04a2eaa9
a1qs <ankiller2007.09.20@gmail.com>
2024-07-25 17:54:11 +0200

    Add feedback for running TeleportToCakeCommand

--- src/main/java/io/github/a1qs/vaultutils/commands/TeleportToCakeCommand.java
+++ src/main/java/io/github/a1qs/vaultutils/commands/TeleportToCakeCommand.java
@@ -11,7 +11,10 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class TeleportToCakeCommand {
@@ -28,16 +31,26 @@ public class TeleportToCakeCommand {
 
     private int tpToCake(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         ServerPlayer player = EntityArgument.getPlayer(context, "player");
+        AtomicBoolean foundInVault = new AtomicBoolean(false);
         ServerVaults.get(player.level).ifPresent((vault) ->
                 vault.ifPresent(Vault.OBJECTIVES, objectives -> {
+                    foundInVault.set(true);
                     if(objectives.get(Objectives.KEY).equals("cake")) {
                         objectives.forEach(CakeObjective.class, objective -> {
                             BlockPos pos = objective.get(CakeObjective.CAKE_POS);
                             player.teleportTo(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+                            context.getSource().sendSuccess(new TextComponent("Teleported " + player.getName().getString() + " to the Cake location"), true);
                             return true;
                         });
+                    } else {
+                        context.getSource().sendFailure(new TextComponent(player.getName().getString() + " is not inside a  Cake Vault!"));
                     }
         }));
+
+        if(!foundInVault.get()) {
+            context.getSource().sendFailure(new TextComponent(player.getName().getString() + " is not inside a Vault!"));
+            return 1;
+        }
 
         return 0;
     }
